d0a8343ec1cd5ec8f9cc5ee28ed84f4fe9c97d5c
Chuang Yu <cyu9960@gmail.com>
2022-08-12 20:32:59 +0800

    change font styles

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,12 +1,16 @@
+local wezterm = require 'wezterm'
+
 return {
   color_scheme = 'Snazzy',
   enable_tab_bar = false,
   window_padding = {
-    left = 1,
-    right = 1,
-    top = 1,
-    bottom = 1
+    left = 2,
+    right = 2,
+    top = 2,
+    bottom = 2
   },
   initial_cols = 100,
   initial_rows = 30,
+  font = wezterm.font('JetBrains Mono'),
+  font_size = 14,
 }
