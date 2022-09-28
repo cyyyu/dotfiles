8a8e0489912a7cb6ed0e1e5b75d238a5a19b8c85
Chuang Yu <cyu9960@gmail.com>
2022-09-28 20:41:12 +0800

    remove c-j/k bindings for telescope

--- nvim.lua
+++ nvim.lua
@@ -208,12 +208,6 @@ require("packer").startup(function()
       require("telescope").setup {
         defaults = {
           file_ignore_patterns = { "node_modules" },
-          mappings = {
-            i = {
-              ["<C-j>"] = require("telescope.actions").move_selection_next,
-              ["<C-k>"] = require("telescope.actions").move_selection_previous,
-            },
-          },
         },
       }
       vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<CR>",
