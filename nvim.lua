b75d22c3ab80c4c4f26ed2dd769e6e98bc2ad843
Chuang Yu <cyu9960@gmail.com>
2023-05-11 23:13:13 +0800

    Remove signature help as it conflicts with backspace

--- nvim.lua
+++ nvim.lua
@@ -399,7 +399,6 @@ require("lazy").setup({
         vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
         vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
         vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, opts)
         vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, opts)
