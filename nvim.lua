730d7d1d31dce06bb1e089cb97298866fa8df49b
Chuang Yu <cyu9960@gmail.com>
2023-07-18 11:07:43 +0800

    Add https://github.com/yamatsum/nvim-cursorline

--- nvim.lua
+++ nvim.lua
@@ -670,4 +670,22 @@ require("lazy").setup({
 	{
 		"prisma/vim-prisma",
 	},
+
+	{
+		"yamatsum/nvim-cursorline",
+		config = function()
+			require("nvim-cursorline").setup({
+				cursorline = {
+					enable = true,
+					timeout = 100,
+					number = false,
+				},
+				cursorword = {
+					enable = true,
+					min_length = 3,
+					hl = { underline = true },
+				},
+			})
+		end,
+	},
 }, {})
