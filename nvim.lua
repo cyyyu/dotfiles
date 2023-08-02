3fbac9e6432b0de31cc3b5f63bfe9c11e665d6ae
Chuang Yu <cyu9960@gmail.com>
2023-08-02 11:36:34 +0800

    Add bufferline

--- nvim.lua
+++ nvim.lua
@@ -294,6 +294,64 @@ require("lazy").setup({
 		end,
 	},
 
+	{
+		"stevearc/dressing.nvim",
+		lazy = true,
+		init = function()
+			---@diagnostic disable-next-line: duplicate-set-field
+			vim.ui.select = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.select(...)
+			end
+			---@diagnostic disable-next-line: duplicate-set-field
+			vim.ui.input = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.input(...)
+			end
+		end,
+	},
+
+	-- fancy tabs
+	{
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+			{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+			{ "[b", "<Cmd>BufferLineCyclePrev<CR>", desc = "Previous buffer" },
+			{ "]b", "<Cmd>BufferLineCycleNext<CR>", desc = "Next buffer" },
+		},
+		opts = {
+			options = {
+      -- stylua: ignore
+      close_command = function(n) require("mini.bufremove").delete(n, false) end,
+      -- stylua: ignore
+      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+				diagnostics = "nvim_lsp",
+				always_show_bufferline = false,
+				diagnostics_indicator = function(_, _, diag)
+					local icons = {
+						Error = " ",
+						Warn = " ",
+						Hint = " ",
+						Info = " ",
+					}
+					local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+						.. (diag.warning and icons.Warn .. diag.warning or "")
+					return vim.trim(ret)
+				end,
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "Neo-tree",
+						highlight = "Directory",
+						text_align = "left",
+					},
+				},
+			},
+		},
+	},
+
 	"mattn/emmet-vim",
 	"sheerun/vim-polyglot",
 
