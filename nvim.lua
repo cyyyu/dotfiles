a2f0999b91e5ea0c74e0374d8424754bc921d763
Chuang Yu <cyu9960@gmail.com>
2023-08-02 13:06:09 +0800

    Add noice.nvim

--- nvim.lua
+++ nvim.lua
@@ -27,8 +27,7 @@ vim.o.ignorecase = true
 -- Use smart case when searching (case sensitive if uppercase)
 vim.o.smartcase = true
 
--- Enable lazyredraw and ttyfast for faster rendering
-vim.o.lazyredraw = true
+-- Enable ttyfast for faster rendering
 vim.o.ttyfast = true
 
 -- Show matching brackets
@@ -812,6 +811,50 @@ require("lazy").setup({
 		keys = {
 			{ "]]", desc = "Next Reference" },
 			{ "[[", desc = "Prev Reference" },
+		},
+	},
+
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			routes = {
+				{
+					filter = {
+						event = "msg_show",
+						any = {
+							{ find = "%d+L, %d+B" },
+							{ find = "; after #%d+" },
+							{ find = "; before #%d+" },
+						},
+					},
+					view = "mini",
+				},
+			},
+			presets = {
+				bottom_search = true,
+				command_palette = true,
+				long_message_to_split = true,
+				inc_rename = true,
+			},
+		},
+		-- stylua: ignore
+		keys = {
+			{ "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+    	{ "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+    	{ "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+    	{ "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+    	{ "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+    	{ "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
+    	{ "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
 		},
 	},
 }, {})
