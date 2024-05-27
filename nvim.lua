4a41bd075dc56edf2423614dbf51b4f154be84d5
Chuang Yu <cyu9960@gmail.com>
2024-05-27 11:56:57 +0800

    patch telescope to 0.1.7

--- nvim.lua
+++ nvim.lua
@@ -214,7 +214,7 @@ require("lazy").setup({
 	},
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.6",
+		tag = "0.1.7",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
