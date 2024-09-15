4c10cad52fd203a1c9f6c5c4a52e7f552ee51f69
Chuang Yu <cyu9960@gmail.com>
2024-09-15 13:47:16 +0800

    fix(nvim): correct LSP server name from "tsserver" to "ts_ls"

--- nvim.lua
+++ nvim.lua
@@ -415,7 +415,7 @@ require("lazy").setup({
 			})
 
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "eslint" },
+				ensure_installed = { "lua_ls", "ts_ls", "eslint" },
 				automatic_installation = true,
 				handlers = {
 					function(server_name)
