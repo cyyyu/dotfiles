8bb8f53d8b712de80fc06f5a0b8d5e347024e163
Chuang Yu <cyu9960@gmail.com>
2025-04-21 12:42:44 +0800

    refactor(nvim): replace formatter.nvim with conform.nvim for code formatting
        * Removed mhartington/formatter.nvim plugin and its setup
        * Added stevearc/conform.nvim plugin for code formatting
        * Configured conform.nvim to use prettierd for various filetypes
        * Updated key mappings for formatting with conform.nvim

--- nvim.lua
+++ nvim.lua
@@ -243,7 +243,6 @@ require("lazy").setup({
       { "hrsh7th/cmp-nvim-lsp" },
       { "williamboman/mason.nvim" },
       { "williamboman/mason-lspconfig.nvim" },
-      { "mhartington/formatter.nvim" }
     },
     init = function()
       -- Reserve a space in the gutter
@@ -277,18 +276,6 @@ require("lazy").setup({
             vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } } })
           end, opts)
           vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
-          vim.api.nvim_set_keymap(
-            "n",
-            "<leader>f",
-            "<cmd>:Format<CR>",
-            { noremap = true, silent = true }
-          )
-          vim.api.nvim_set_keymap(
-            "n",
-            "<leader>F",
-            "<cmd>:FormatWrite<CR>",
-            { noremap = true, silent = true }
-          )
         end,
       })
 
@@ -667,23 +654,27 @@ require("lazy").setup({
     "Bekaboo/dropbar.nvim",
   },
 
-  -- {
-  -- "Exafunction/codeium.vim",
-  -- config = function()
-  -- vim.keymap.set("i", "<C-j>", function()
-  -- return vim.fn["codeium#Accept"]()
-  -- end, { expr = true })
-  -- vim.keymap.set("i", "<c-;>", function()
-  -- return vim.fn["codeium#CycleCompletions"](1)
-  -- end, { expr = true })
-  -- vim.keymap.set("i", "<c-,>", function()
-  -- return vim.fn["codeium#CycleCompletions"](-1)
-  -- end, { expr = true })
-  -- vim.keymap.set("i", "<c-x>", function()
-  -- return vim.fn["codeium#Clear"]()
-  -- end, { expr = true })
-  -- end,
-  -- },
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          javascript = { "prettierd" },
+          typescript = { "prettierd" },
+          javascriptreact = { "prettierd" },
+          typescriptreact = { "prettierd" },
+        },
+      })
+
+      vim.keymap.set({ "n", "v" }, "<leader>f", function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end, { desc = "Format file or range (in visual mode)" })
+    end,
+  },
 
   {
     "github/copilot.vim",
@@ -818,122 +809,3 @@ end
 
 -- Map the function to the "=" key in normal mode
 vim.api.nvim_set_keymap("n", "t", "<cmd>lua ToggleSmartFold()<CR>", { noremap = true, silent = true })
-
--- Utilities for creating configurations
-local util = require "formatter.util"
-
--- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-require("formatter").setup {
-  -- Enable or disable logging
-  logging = false,
-  -- Set the log level
-  log_level = vim.log.levels.WARN,
-  -- All formatter configurations are opt-in
-  filetype = {
-    -- Formatter configurations for filetype "lua" go here
-    -- and will be executed in order
-    lua = {
-      -- "formatter.filetypes.lua" defines default configurations for the
-      -- "lua" filetype
-      require("formatter.filetypes.lua").stylua,
-
-      -- You can also define your own configuration
-      function()
-        -- Supports conditional formatting
-        if util.get_current_buffer_file_name() == "special.lua" then
-          return nil
-        end
-
-        -- Full specification of configurations is down below and in Vim help
-        -- files
-        return {
-          exe = "stylua",
-          args = {
-            "--search-parent-directories",
-            "--stdin-filepath",
-            util.escape_path(util.get_current_buffer_file_path()),
-            "--",
-            "-",
-          },
-          stdin = true,
-        }
-      end
-    },
-
-
-    javascript = {
-      -- prettierd
-      function()
-        return {
-          exe = "prettierd",
-          args = { vim.api.nvim_buf_get_name(0) },
-          stdin = true
-        }
-      end
-    },
-    typescript = {
-      function()
-        return {
-          exe = "prettierd",
-          args = { vim.api.nvim_buf_get_name(0) },
-          stdin = true
-        }
-      end
-    },
-    typescriptreact = {
-      function()
-        return {
-          exe = "prettierd",
-          args = { vim.api.nvim_buf_get_name(0) },
-          stdin = true
-        }
-      end
-    },
-    javascriptreact = {
-      function()
-        return {
-          exe = "prettierd",
-          args = { vim.api.nvim_buf_get_name(0) },
-          stdin = true
-        }
-      end
-    },
-    json = {
-      function()
-        return {
-          exe = "prettierd",
-          args = { vim.api.nvim_buf_get_name(0) },
-          stdin = true
-        }
-      end
-    },
-    html = {
-      function()
-        return {
-          exe = "prettierd",
-          args = { vim.api.nvim_buf_get_name(0) },
-          stdin = true
-        }
-      end
-    },
-    css = {
-      function()
-        return {
-          exe = "prettierd",
-          args = { vim.api.nvim_buf_get_name(0) },
-          stdin = true
-        }
-      end
-    },
-
-    -- Use the special "*" filetype for defining formatter configurations on
-    -- any filetype
-    ["*"] = {
-      -- "formatter.filetypes.any" defines default configurations for any
-      -- filetype
-      require("formatter.filetypes.any").remove_trailing_whitespace,
-      -- Remove trailing whitespace without 'sed'
-      -- require("formatter.filetypes.any").substitute_trailing_whitespace,
-    }
-  }
-}
